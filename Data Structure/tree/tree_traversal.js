785845b9ee1182b9a55a362f4977d61db740530b
Dohee <dohee0203z@skku.edu>
2021-03-25 01:52:32 +0900

    update tree

---
+++ Data Structure/tree/tree_traversal.js
@@ -0,0 +1,90 @@
+// 트리 정의하기
+class Tree {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+
+  setVal(val) {
+    this.val = val;
+  }
+
+  setLeft(node) {
+    this.left = node;
+  }
+
+  setRight(node) {
+    this.right = node;
+  }
+}
+
+// 부모, 자식 관계 정의
+let root = new Tree(1);
+let node2 = new Tree(2);
+let node3 = new Tree(3);
+let node4 = new Tree(4);
+let node5 = new Tree(5);
+let node6 = new Tree(6);
+let node7 = new Tree(7);
+root.setLeft(node2);
+root.setRight(node3);
+root.left.setLeft(node4);
+root.left.setRight(node5);
+root.right.setLeft(node6);
+root.right.setRight(node7);
+
+var PreOrderTraversal = function (node) {
+  let stack = [];
+  stack.push(node);
+  while (stack.length > 0) {
+    let pop_node = stack.pop();
+    console.log(pop_node.val);
+    if (pop_node.right) stack.push(pop_node.right);
+    if (pop_node.left) stack.push(pop_node.left);
+  }
+};
+
+var InOrderTraversal = function (node) {
+  let crnt_node = node;
+  let stack = [];
+  while (true) {
+    if (crnt_node != null) {
+      stack.push(crnt_node);
+      crnt_node = crnt_node.left;
+    } else if (stack.length > 0) {
+      crnt_node = stack.pop();
+      console.log(crnt_node.val);
+      crnt_node = crnt_node.right;
+    } else {
+      break;
+    }
+  }
+};
+
+var PostOrderTraversal = function (node) {
+  let crnt_node = node;
+  let stack = [];
+  let last_visit_node = null;
+  while (true) {
+    if (crnt_node != null) {
+      stack.push(crnt_node);
+      crnt_node = crnt_node.right;
+    } else if (stack.length > 0) {
+      peek_node = stack[stack.length - 1];
+      if (peek_node.left != null && last_visit_node != peek_node.left) {
+        crnt_node = peek_node.left;
+      } else {
+        console.log(peek_node.val);
+        last_visit_node = stack.pop();
+      }
+    } else {
+      break;
+    }
+  }
+};
+PreOrderTraversal(root);
+console.log("---------");
+InOrderTraversal(root);
+console.log("---------");
+PostOrderTraversal(root);
