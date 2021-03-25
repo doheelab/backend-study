8560cb9f19b1310aaa7e67fa01ab72e30232c07c
Dohee <dohee0203z@skku.edu>
2021-03-25 22:25:17 +0900

    update binary tree

--- Data Structure/binary_tree/tree_traversal(reculsive).js
+++ Data Structure/binary_tree/tree_traversal(reculsive).js
@@ -34,6 +34,15 @@ root.leftNode.setRight(node5);
 root.rightNode.setLeft(node6);
 root.rightNode.setRight(node7);
 
+// in-order
+var recursiveInOrder = function (node) {
+  if (!node) {
+    return;
+  }
+  this.recursiveInOrder(node.leftNode);
+  console.log(node.val);
+  this.recursiveInOrder(node.rightNode);
+};
 
 // pre-order
 var recursivePreOrder = function (node) {
@@ -43,17 +52,6 @@ var recursivePreOrder = function (node) {
   console.log(node.val);
   this.recursivePreOrder(node.leftNode);
   this.recursivePreOrder(node.rightNode);
-};
-
-
-// in-order
-var recursiveInOrder = function (node) {
-  if (!node) {
-    return;
-  }
-  this.recursiveInOrder(node.leftNode);
-  console.log(node.val);
-  this.recursiveInOrder(node.rightNode);
 };
 
 // post-order
